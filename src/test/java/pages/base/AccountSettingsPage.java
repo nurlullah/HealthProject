342ec320481be1dd335d2b788a263f052340f4bd
Nurullah Demirkoparan <nurlullahhh@gmail.com>
2022-10-21 14:10:40 +0300

    page name update

--- src/test/java/pages/base/AccountSettingsPage.java
+++ src/test/java/pages/base/AccountSettingsPage.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class UserSettingsPage {
-    public UserSettingsPage() {
+public class AccountSettingsPage {
+    public AccountSettingsPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
