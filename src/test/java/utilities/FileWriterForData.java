baf398019cff769f68421f148b9fe6b26aadf58f
Mualla Yarar <muallayarar@gmail.com>
2022-10-23 19:25:12 +0300

    updated

--- src/test/java/utilities/FileWriterForData.java
+++ src/test/java/utilities/FileWriterForData.java
@@ -16,11 +16,8 @@ public class FileWriterForData {
 
         try {
             //Bu verilen adreste benim için txt dosyası oluşturuyor
-<<<<<<< Updated upstream
-            FileWriter fileWriter=new FileWriter(ConfigReader.getProperty("registrantData"), true);//true dersek önceki datayı silmez, false dersek siler
-=======
             FileWriter fileWriter=new FileWriter("src/test/resources/testdata/PatientData.feature", false);//true dersek önceki datayı silmez iki kez yazmış olur, false dersek siler
->>>>>>> Stashed changes
+
             //Data yazdırabiliriz verilen dosya adresinde
             BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
             bufferedWriter.append(registrant.toString()+"\n");
